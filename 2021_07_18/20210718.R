1e3a4084f42b29c4cc30a614330eaf12c86591ff
kyuyounglee <pia222.kr@gmail.com>
2021-07-18 16:23:34 +0900

    Add files via upload

--- 2021_07_18/20210718.R
+++ 2021_07_18/20210718.R
@@ -135,5 +135,161 @@ datadf<- readcsv("E:/RData/Part-I/emp2.csv","mac")
 2^3
 
 # 4 단 출력
+gugudan <- function(dan){
+  for (index in c(1:9)) {
+    cat(dan," x ",index," = ",dan*index,"\n")
+  }  
+}
 
 
+# 2 ~ 9 단까지 출력
+
+# gugudan(2)   ~  gugudan(9)
+
+for(index in c(2:9)){
+  gugudan(index)
+  cat("----------------------------------\n")
+}
+
+source("E:/RData/Part-I/myutil.R")
+datadf<- rcsv("E:/RData/Part-I/emp2.csv","win")
+
+
+test <- read.csv("E:/RData/Part-I/test.csv",header = T)
+head(test,2)
+
+#요약통계량
+summary(test)
+#빈도수
+table(test$A)
+#시각화
+barplot(table(test$A),col = rainbow(5))
+
+
+# test data.fram 의 각 컬럼에 대한 빈도수를 구해보자
+table(test$A)
+table(test[1])
+
+1:length(names(test))
+
+
+
+names(test)[4]
+
+
+barTest<-function(){
+  test <- read.csv("E:/RData/Part-I/test.csv",header = T)
+  colCount<-length(names(test))
+  par(mfrow = c(2,3))
+  for(index in 1:colCount){
+    print(table(test[index]))
+    barplot( table(test[index])
+             ,col=rainbow(colCount),main=names(test)[index]  
+    )
+  }
+}
+
+barTest()
+
+
+x<-c(7,5,12,9,15,6)
+length(x)
+sum(x - mean(x)/2) / length(x)-1
+
+Sys.setlocale("LC_ALL","korean")
+
+source("E:/RData/Part-I/myutil.R")
+var_sd(x)
+
+
+titanic<-read.csv("E:/RData/Part-I/train.csv",header = T)
+head(titanic)
+summary(titanic)
+
+mean(titanic$Age,na.rm = T)
+
+# 결측치를 제거하는 방법
+# 1. 해당 값을 제거하는 방법( 실제 제거, 통계량을 구할때 계산에 미포함)
+# 2. N/A  를 0으로 대처하거나. .평균으로 대처
+
+
+is.na(titanic$Age)
+
+x<- c(1,2,NA,NA,5)
+!is.na(x)
+x[!is.na(x)]
+
+# NA를 찾아서 0으로 대체
+
+ifelse(is.na(x),0,x)
+
+avg<-mean(titanic$Age,na.rm = T)
+
+titanic$NotNAage2<- ifelse(is.na(titanic$Age),avg,titanic$Age)
+summary(titanic)
+
+str(titanic$Age)
+
+
+# 891
+length(titanic$Age) 
+
+# 714
+length(titanic$Age[!is.na(titanic$Age)])
+
+titanic$test<-titanic$Age[!is.na(titanic$Age)]
+
+# is.na()는 결측치를 true로 보고
+# !is.na()는 결측치를 false로 보고 그래서 베터에 []안에 넣으면
+# 결측치가 제거된 데이터셋을 얻는다... 그러나. 기존
+# data.frame에 추가를 못한다. (why?? row의 갯수가 안 맞아서)
+
+# 난수생성  실수
+runif(10,min=0,max=100)
+
+# 난수생성  정수
+str(sample(1:100,size=10))
+
+v<-sample(1:100,size=10)
+# [1]  53 100  64  65  85
+v[1]
+v[c(TRUE,FALSE,FALSE,TRUE,FALSE)]
+
+
+# 벡터에서 60보다 큰 수를 추출
+v + 10
+v > 60
+v[c(v > 60)]
+
+# 벡터에서 60보다 큰 수는 0으로 대처
+ifelse(v > 60,0,v)
+
+# 벡터에서 60보다 큰 수는 평균값으로 대처
+ifelse(v>60, mean(v), v)
+
+age<-sample(1:100,size=10)
+gender<-sample(0:100,size=10)
+gender<-gender %% 2
+gender<-ifelse(gender == 0,"woman","man")
+# 0 woman
+# 1 man
+# 0 0 0  1 1 0 1 0 1 0
+
+df<-data.frame(age,gender)
+df$agetype<-0
+
+summary(titanic)
+titanic$AgeType<- ifelse(titanic$Age>19,"adult","child")
+titanic$AgeType<-as.factor(titanic$AgeType)
+
+titanic$Embarked<-as.factor(titanic$Embarked)
+
+table(titanic$AgeType)
+table(titanic$Sex)
+table(titanic$Survived)
+
+
+# 이어서서
+par(mfrow = c(1,1))
+titanic$AgeType<- ifelse(is.na(titanic$AgeType),"NON",titanic$AgeType)
+barplot(table(titanic$AgeType),table(titanic$Survived))
--- 2021_07_18/myutil.R
+++ 2021_07_18/myutil.R
@@ -1,5 +1,5 @@
 # os --> mac
-readcsvWinOrMac<-function(filepath,os="win"){
+rcsv<-function(filepath,os="mac"){
   # 리턴값을 저장할 df 변수 정의
   df<- data.frame()
   #조건문 os 파라메터값이 mac 과 win일때의 로직일 다름
@@ -7,10 +7,42 @@ readcsvWinOrMac<-function(filepath,os="win"){
     df<-read.csv(filepath
                  ,fileEncoding = "CP949",encoding = "UTF-8"
                  ,header = T )  
-  }else if(os=="win"){
+  }else{
     df<-read.csv(filepath,header = T )  
   }
   # 최종 결과값을 함수를 호출한 곳으로 df 값을 전달함함
   return (df)
 }
+
+# 분산과 표준편차
+# param 
+#       x : 벡터
+var_sd<-function(x){
+  #표본분산
+  var<-sum(x - mean(x)/2) / length(x)-1
+  sd<-sqrt(var)
+  cat("var : ",var,"\n")
+  cat("sd : ",sd)
+}
+
+# is.na()는 결측치를 true로 보고
+# !is.na()는 결측치를 false로 보고 그래서 베터에 []안에 넣으면
+# 결측치가 제거된 데이터셋을 얻는다... 그러나. 기존
+# data.frame에 추가를 못한다. (why?? row의 갯수가 안 맞아서)
+rmna<- function(x){
+  return (x[!is.na(x)])
+}
+
+# 원본 data.fram에 컬럼을 추가 na은 0으로
+# df    ,colname    ,addcolname
+# 원본    찾을컬럼     추가할 컬럼
+replacena0<-function(df,colname,addcolname){
+  newcol<- ifelse(is.na(df$colname),0,df$colname)
+  df$addcolname<- newcol
+}
+
+
+
+
+
 
